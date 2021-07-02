ab2fa3aa5017affafd68aa9ed1c2e47521f1c644
Jonathan Slauter <jonathanslauter@gmail.com>
2021-07-02 14:53:30 -0700

    initial stylings added

--- seeds/index.js
+++ seeds/index.js
@@ -1,34 +1,39 @@
 const mongoose = require('mongoose');
 const cities = require('./cities');
 const Campground = require('../models/campground');
-const {places, descriptors} = require('./seedHelpers')
+const { places, descriptors } = require('./seedHelpers');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-    console.log("Database connected");
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Database connected');
 });
 
-const sample = (array) => array[Math.floor(Math.random() * array.length)]
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
-    await Campground.deleteMany({})
-    for(let i = 0; i < 50; i++){
-        const random1000 = Math.floor(Math.random() * 1000)
-        const camp = new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)} `
-        })
-        await camp.save()
-    }
-}
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)} `,
+      image: 'https://source.unsplash.com/collection/483251',
+      description:
+        "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
+      price,
+    });
+    await camp.save();
+  }
+};
 
-seedDB().then(() =>{
-    mongoose.connection.close()
-})
+seedDB().then(() => {
+  mongoose.connection.close();
+});
