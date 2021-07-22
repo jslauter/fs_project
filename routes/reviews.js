dca1240ba9c93ff28893845c5730eae7214d054d
Jonathan Slauter <jonathanslauter@gmail.com>
2021-07-22 15:41:13 -0700

    session and flash added

--- routes/reviews.js
+++ routes/reviews.js
@@ -20,12 +20,15 @@ const validateReview = (req, res, next) => {
     }
 }
 
-  router.post('/', validateReview, catchAsync(async (req, res) => {
+
+
+router.post('/', validateReview, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
     campground.reviews.push(review);
     await review.save();
     await campground.save();
+    req.flash('success', 'Created new review!');
     res.redirect(`/campgrounds/${campground._id}`);
 }))
 
@@ -33,6 +36,7 @@ router.delete('/:reviewId', catchAsync(async (req, res) => {
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     await Review.findByIdAndDelete(reviewId);
+    req.flash('success', 'Successfully deleted review')
     res.redirect(`/campgrounds/${id}`);
 }))
 
